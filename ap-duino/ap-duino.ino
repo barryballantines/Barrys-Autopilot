6c97fa9c99f29421cd1cc65a872a30ee8aa2f6b8
Barry Ballantines <barry.ballantines@mail.de>
2017-05-09 21:26:25 +0200

    Initial sketch for ap-duino - just for testing (yet)

---
+++ ap-duino/ap-duino.ino
@@ -0,0 +1,19 @@
+
+#include "AutoPilot.h"
+
+AutoPilot ap;
+
+void setup() {
+  // put your setup code here, to run once:
+  ap.setHeading(180);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int hdg = ap.getHeading();
+  hdg = hdg + 5;
+  ap.setHeading(hdg);
+  ap.updateDisplay();
+  delay(1000);
+}
